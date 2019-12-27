5bf0994b7e24d5623635e328db19ac1fc8cba7a1
Denis Feklushkin <denis.feklushkin@gmail.com>
2019-12-27 15:21:53 +0700

    dashboard measurements

--- source/app.d
+++ source/app.d
@@ -23,11 +23,11 @@ void main(string[] args)
 
             Measurement[] meteo;
 
-            if(!o.meteo.t.isNull) meteo ~= Measurement("t_", "deg", o.meteo.t.get);
-            if(!o.meteo.ws.isNull) meteo ~= Measurement("ws_", "м/с", o.meteo.ws.get);
-            if(!o.meteo.wd.isNull) meteo ~= Measurement("wd_", "deg", o.meteo.wd.get);
-            if(!o.meteo.hum.isNull) meteo ~= Measurement("hum_", "%", o.meteo.hum.get);
-            if(!o.meteo.p.isNull) meteo ~= Measurement("p_", "мм.рт.ст.", o.meteo.p.get);
+            if(!o.meteo.t.isNull) meteo ~= Measurement("Температура воздуха__", "deg", o.meteo.t.get);
+            if(!o.meteo.ws.isNull) meteo ~= Measurement("Скорость ветра__", "м/с", o.meteo.ws.get);
+            if(!o.meteo.wd.isNull) meteo ~= Measurement("Направление ветра__", "deg", o.meteo.wd.get);
+            if(!o.meteo.hum.isNull) meteo ~= Measurement("Влажность воздуха__", "%", o.meteo.hum.get);
+            if(!o.meteo.p.isNull) meteo ~= Measurement("Атм. давление__", "мм.рт.ст.", o.meteo.p.get);
 
             const meteoTime = measurementsOfObservatory.maxElement!((a) => a.dateTime).dateTime;
 
