bde45a7ced16cc13de1a36175398c56d6beff4fa
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-09-07 08:28:17 +0700

    moved to dpq2 with query generator

--- source/pgdb.d
+++ source/pgdb.d
@@ -2,7 +2,7 @@ module krasecodump.pgdb;
 
 import krasecodump.grab;
 import vibe.db.postgresql;
-import db_helper.db.util;
+import dpq2.query_gen;
 import std.conv: to;
 import std.datetime: SysTime;
 import std.exception: enforce;
@@ -10,11 +10,11 @@ import std.typecons: Nullable;
 
 private Nullable!int selectPlace(Connection conn, Coords coords, string name)
 {
-    auto qp = statementWrapper(
-        `SELECT place_id `,
-        `FROM places `,
-        `WHERE `, u("lat", coords.lat),
-        `AND `, u("lon", coords.lon),
+    auto qp = wrapStatement(
+        `SELECT place_id`,
+        `FROM places`,
+        `WHERE`, u("lat", coords.lat),
+        `AND`, u("lon", coords.lon),
         `LIMIT 1`
     );
 
@@ -42,14 +42,14 @@ private int upsertPlace(Connection conn, Coords coords, string name)
 // При каждом вызове вызывает приращение sequence
 private int upsertPlaceHurtsSequence(Connection conn, Coords coords, string name)
 {
-    auto qp = statementWrapper(
+    auto qp = wrapStatement(
         `INSERT INTO places (`,
             i("lat", coords.lat),
             i("lon", coords.lon),
             i("place_name", name),
-        `) VALUES(`, Dollars(), `) `~
-        `ON CONFLICT (lat, lon) `~
-        `DO UPDATE SET lat = EXCLUDED.lat `~ // just for ensure what RETURNING always returns value
+        `) VALUES(`, Dollars(), `)`,
+        `ON CONFLICT (lat, lon)`,
+        `DO UPDATE SET lat = EXCLUDED.lat`, // just for ensure what RETURNING always returns value
         `RETURNING place_id`
     );
 
@@ -61,12 +61,12 @@ private int upsertPlaceHurtsSequence(Connection conn, Coords coords, string name
 
 private Nullable!int selectSubstance(Connection conn, string name, string unit, double pdk)
 {
-    auto qp = statementWrapper(
-        `SELECT substance_id `,
-        `FROM substances `,
-        `WHERE `, u("substance_name", name),
-        `AND `, u("unit", unit),
-        `AND `, u("pdk", pdk),
+    auto qp = wrapStatement(
+        `SELECT substance_id`,
+        `FROM substances`,
+        `WHERE`, u("substance_name", name),
+        `AND`, u("unit", unit),
+        `AND`, u("pdk", pdk),
         `LIMIT 1`
     );
 
@@ -94,14 +94,14 @@ private int upsertSubstance(Connection conn, string name, string unit, double pd
 // При каждом вызове вызывает приращение sequence
 private int upsertSubstanceHurtsSequence(Connection conn, string name, string unit, double pdk)
 {
-    auto qp = statementWrapper(
+    auto qp = wrapStatement(
         `INSERT INTO substances (`,
             i("substance_name", name),
             i("unit", unit),
             i("pdk", pdk),
-        `) VALUES(`, Dollars(), `) `~
-        `ON CONFLICT (substance_name, unit, pdk) `~
-        `DO UPDATE SET pdk = EXCLUDED.pdk `~ // just for ensure what RETURNING always returns value
+        `) VALUES(`, Dollars(), `)`,
+        `ON CONFLICT (substance_name, unit, pdk)`,
+        `DO UPDATE SET pdk = EXCLUDED.pdk`, // just for ensure what RETURNING always returns value
         `RETURNING substance_id`
     );
 
@@ -126,15 +126,15 @@ private void upsertMeasurement(Connection conn, in SysTime time, short placeId, 
         m.pdk.isNull ? PDK_ISNT_SET : m.pdk.get
     );
 
-    auto qp = statementWrapper(
+    auto qp = wrapStatement(
         `INSERT INTO measurements (`,
             i("place_id", placeId),
             i("measurement_time", m.dateTime),
             i("substance_id", substanceId),
             i("value", m.value),
             i("recorded_time", time),
-        `) VALUES(`, Dollars(), `) `~
-        `ON CONFLICT (place_id, measurement_time, substance_id, value) `~
+        `) VALUES(`, Dollars(), `)`,
+        `ON CONFLICT (place_id, measurement_time, substance_id, value)`,
         `DO NOTHING`
     );
 
@@ -155,4 +155,55 @@ void upsertMeasurementsToDB(PostgresClient client, in SysTime time, in Coords co
                 conn.upsertMeasurement(time, placeId, m);
         }
     );
+}
+
+private void checkOneRowResult(immutable Answer r) {
+  if(r.length != 1)
+    throw new SearchException(ExceptionType.NOT_ONE_ROW, r);
+}
+
+///
+enum ExceptionType : ubyte {
+  USER_NOT_FOUND = 0, ///
+  ZERO_ROWS = 0, ///
+  NOT_ONE_ROW, ///
+  MORE_THAN_ONE_ROW, ///
+}
+
+class SearchException : Exception {
+  const ExceptionType type;
+  immutable Answer answer = null;
+
+  /// for checkOneRow only
+  this(ExceptionType t, immutable Answer a, string file = __FILE__, size_t line = __LINE__) {
+    answer = a;
+
+    this(t, answer.length, file, line);
+  }
+
+  //TODO: remove it
+  ///
+  this(ExceptionType t, long rowsNum, string file = __FILE__, size_t line = __LINE__) pure @safe {
+    type = t;
+
+    super(
+      type.createExceptionMsg~`, not ` ~ rowsNum.to!string,
+      file,
+      line
+    );
+  }
+}
+
+private string createExceptionMsg(ExceptionType t) pure @safe {
+    with(ExceptionType)
+    final switch(t) {
+      case ZERO_ROWS:
+        return `Zero rows affected`;
+
+      case NOT_ONE_ROW:
+        return `Strictly one row should be affected`;
+
+      case MORE_THAN_ONE_ROW:
+        return `Not more than one row should be affected`;
+    }
 }
